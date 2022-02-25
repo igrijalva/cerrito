6af0b9c9098b9a7b6e04053af100480d7ff84ef0
igrijalva <grijalva.israel@gmail.com>
2022-02-25 00:44:42 -0600

    Se agrega servicio de dispositivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { DevicesService } from './devices.service';
+import { Device } from './interfaces/device';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  constructor(private devicesService: DevicesService) {}
+
+  ngOnInit(): void {
+    this.devices = this.devicesService.getDevices();
+  }
+
+  devices: Device[] = [];
   name = 'Angular ' + VERSION.major;
 }
--- src/app/devices.service.ts
+++ src/app/devices.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Device } from './interfaces/device';
 
 @Injectable()
 export class DevicesService {
   constructor() {}
 
-  getDevices() {}
+  getDevices(): Device[] {
+    const devices = [
+      { id: 1, name: 'Entrada', pin: 17 },
+      { id: 2, name: 'Marquesina', pin: 18 },
+      { id: 3, name: 'Pasillo', pin: 19 },
+      { id: 4, name: 'Isra', pin: 20 },
+      { id: 5, name: 'Arbol', pin: 21 },
+    ];
+
+    return devices;
+  }
 }
